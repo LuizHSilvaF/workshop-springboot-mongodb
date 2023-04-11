82f8f100aeefd3e1c052a7ab0e52e28c86931458
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2023-04-11 13:12:14 -0300

    User insert with POST

--- src/main/java/com/luiz/workshopmongo/service/UserService.java
+++ src/main/java/com/luiz/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luiz.workshopmongo.domain.User;
+import com.luiz.workshopmongo.dto.UserDTO;
 import com.luiz.workshopmongo.repository.UserRepository;
 import com.luiz.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -24,4 +25,12 @@ public class UserService {
 		Optional<User>obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 		}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
