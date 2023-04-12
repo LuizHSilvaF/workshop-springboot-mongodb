5f293e221a27afa4efeef27bbd7e6a7e173c8fbc
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2023-04-12 13:47:58 -0300

    Comments to posts

--- src/main/java/com/luiz/workshopmongo/config/Instantiation.java
+++ src/main/java/com/luiz/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.luiz.workshopmongo.config;
 
 
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import com.luiz.workshopmongo.domain.Post;
 import com.luiz.workshopmongo.domain.User;
 import com.luiz.workshopmongo.dto.AuthorDTO;
+import com.luiz.workshopmongo.dto.CommentDTO;
 import com.luiz.workshopmongo.repository.PostRepository;
 import com.luiz.workshopmongo.repository.UserRepository;
 
@@ -25,6 +27,8 @@ public class Instantiation implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
 		userRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
@@ -38,11 +42,21 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, "21/03/2023", "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, "27/03/2023", "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano", sdf.parse("21/03/2023"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2023"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2023"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
+		
+		
+		
 	}
 
 }
