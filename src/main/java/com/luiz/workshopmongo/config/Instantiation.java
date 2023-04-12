1473a52acada396db7d9aca7d98e08d68ffd64d8
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2023-04-12 12:59:16 -0300

    AuthorDTO

--- src/main/java/com/luiz/workshopmongo/config/Instantiation.java
+++ src/main/java/com/luiz/workshopmongo/config/Instantiation.java
@@ -4,13 +4,13 @@ package com.luiz.workshopmongo.config;
 
 import java.util.Arrays;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.luiz.workshopmongo.domain.Post;
 import com.luiz.workshopmongo.domain.User;
+import com.luiz.workshopmongo.dto.AuthorDTO;
 import com.luiz.workshopmongo.repository.PostRepository;
 import com.luiz.workshopmongo.repository.UserRepository;
 
@@ -35,8 +35,8 @@ public class Instantiation implements CommandLineRunner{
 		
 		postRepository.deleteAll();
 		
-		Post post1 = new Post(null, "21/03/2023", "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, "27/03/2023", "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, "21/03/2023", "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, "27/03/2023", "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
