0973e25ae680d33413146af4cc740143057667a1
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2023-04-12 13:16:52 -0300

    Endpoint return posts

--- src/main/java/com/luiz/workshopmongo/resources/UserResource.java
+++ src/main/java/com/luiz/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.luiz.workshopmongo.domain.Post;
 import com.luiz.workshopmongo.domain.User;
 import com.luiz.workshopmongo.dto.UserDTO;
 import com.luiz.workshopmongo.service.UserService;
@@ -60,4 +61,9 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@RequestMapping(value="/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
