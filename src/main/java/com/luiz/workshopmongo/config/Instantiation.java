3b98206c165e35f05aea8c7ff0ddbe87c468ee1b
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2023-04-12 12:46:04 -0300

    Entity Post

--- src/main/java/com/luiz/workshopmongo/config/Instantiation.java
+++ src/main/java/com/luiz/workshopmongo/config/Instantiation.java
@@ -1,12 +1,17 @@
 package com.luiz.workshopmongo.config;
 
+
+
 import java.util.Arrays;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.luiz.workshopmongo.domain.Post;
 import com.luiz.workshopmongo.domain.User;
+import com.luiz.workshopmongo.repository.PostRepository;
 import com.luiz.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -14,6 +19,8 @@ public class Instantiation implements CommandLineRunner{
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired PostRepository postRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -25,6 +32,13 @@ public class Instantiation implements CommandLineRunner{
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		postRepository.deleteAll();
+		
+		Post post1 = new Post(null, "21/03/2023", "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, "27/03/2023", "Bom dia", "Acordei feliz hoje!", maria);
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
 }
